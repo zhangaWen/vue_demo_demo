baa0c9bfca11717405a48997ad1af6dc054810a5
awen <awen@163.com>
2019-07-31 22:37:07 +0800

    'axios发ajax请求---创建事件总线对象'

--- src/main.js
+++ src/main.js
@@ -2,8 +2,16 @@
  * 入口js
  */
 import Vue from 'vue'
+import VueResource from 'vue-resource'
+import axios from 'axios'
 import App from './App.vue'
 /* eslint-disable no-new */
+Vue.use(VueResource)
+
+
+//创建事件总线对象，并保存到Vue的原型对象
+Vue.prototype.$bus = new Vue()
+Vue.prototype.$axios = axios
 new Vue({
   el: '#app',
   components: {
