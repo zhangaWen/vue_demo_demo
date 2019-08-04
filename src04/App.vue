77173e7edeeca244a95e918c6a9337861fe8e108
awen <awen@163.com>
2019-08-04 10:19:45 +0800

    '复习'

---
+++ src04/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <Search/>
+    <Main/>
+  </div>
+</template>
+<script type="text/ecmascript-6">
+  import Main from './components/Main'
+  import Search from './components/Search'
+  export default {
+    components: {
+      Search,
+      Main
+    }    
+  }
+</script>
+<style scoped>
+
+</style>
